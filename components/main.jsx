88dd91ec646f8b236975507c3d357f2fa68511fc
David <david.nunez@oktana.io>
2021-08-30 07:43:27 -0400

    Initial timeout implementation

---
+++ components/main.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+
+import {Button, Container, Row, Col} from 'react-bootstrap';
+
+const Main = ({setHeaderPrefix}) => {
+    const [timer, setTimer] = useState({min:25, sec: 0});
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
+    useEffect(() => {
+        if(isTimerRunning){
+            setHeaderPrefix(`${timer.min}:${timer.sec.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping:false})}`);
+            setTimeout( ()=>{
+                const {min, sec} = timer;
+                if(min === sec && sec === 0){
+                    setIsTimerRunning(false);
+                }else if(sec === 0){
+                    setTimer({
+                        ...timer,
+                        min: min -1,
+                        sec: 59
+                    });
+                }else{
+                    setTimer({
+                        ...timer,
+                        sec: sec - 1,
+                    });
+                }
+            }, 1000)
+        }
+    }, [timer, isTimerRunning])
+
+    const handleStopTimer = () => {
+        setIsTimerRunning(false);
+    }
+    const handleStartTimer = () => {
+        setIsTimerRunning(true);
+    }
+    return(
+        <Container className="justify-content-center">
+            <Row>
+                <Col sm="12" className=" mt-5">
+                    <h1 className="text-center">Pomodoro app</h1>
+                </Col>
+                <Col className="my-3">
+                    <div className="text-center">
+                        <span className="h4 p-3 border border-info rounded">{`${timer.min}:${timer.sec.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping:false})}`}</span>
+                    </div>
+                </Col>
+                <Col sm="12" className=" mt-3">
+                    <div className="text-center">
+                        <Button variant="success" onClick={handleStartTimer}>Start timer</Button>{` `}
+                        <Button variant="danger" onClick={handleStopTimer}>Stop timer</Button>
+                    </div>
+                </Col>
+                
+            </Row>
+        </Container>
+    );
+}
+export default Main;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [headerPrefix, setHeaderPrefix] = useState('');
+  return <>
+    <Head>    
+        <title>{headerPrefix + ` `}Pomodoro App!</title>
+        <meta name="description" content="Pomodoro app" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} setHeaderPrefix={setHeaderPrefix} />
+  </>
 }
 
 export default MyApp
