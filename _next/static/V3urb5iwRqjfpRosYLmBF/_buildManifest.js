188da6e167323ee9d3c26c612e185b068e7614b4
davinun99 <nundavid99@gmail.com>
2021-08-31 22:35:55 +0000

    Deploying to gh-pages from @ davinun99/pomodoro-app@4ee13ae7af9eac587860b71453dcdc49daba4adf 🚀

---
+++ _next/static/V3urb5iwRqjfpRosYLmBF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-86b5f9345b9be4e4f6b4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
